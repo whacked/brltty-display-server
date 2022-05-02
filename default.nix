9c232f5382f266f420aedd3969b961b4cdede243
whacked-xps <whacked@users.noreply.github.com>
2022-05-02 10:42:16 +0800

    update nix env to use ~/setup commons; update requirements.txt

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
 with import <nixpkgs> {};
 
-stdenv.mkDerivation rec {
+let 
+  nixShortcutsPath = ~/setup/bash/nix_shortcuts.sh;
+  nixShortcuts = (import nixShortcutsPath);
+in stdenv.mkDerivation rec {
     env = buildEnv {
         name = name;
         paths = buildInputs;
@@ -9,30 +12,28 @@ stdenv.mkDerivation rec {
     name = "braille-python-server";
     buildInputs = [
         brltty
-        python38
-        python38Packages.ipython
+        python39
+        python39Packages.ipython
+        glibcLocales
+    ] ++ nixShortcuts.buildInputs;
+
+    nativeBuildInputs = [
+      nixShortcutsPath
     ];
 
     shellHook = ''
         if [ ! -e brltty.conf ]; then
             touch brltty.conf
         fi
-        if [ "x$VIRTUAL_ENV" == "x" ]; then
-            if [ "x$USERCACHE" != "x" ]; then
-                export VIRTUAL_ENV="$USERCACHE/.virtualenvs/${name}"
-            fi
-        fi
-        if [ ! -e $VIRTUAL_ENV ]; then
-            echo creating virtualenv at $VIRTUAL_ENV
-            python -m venv $VIRTUAL_ENV
-            source $VIRTUAL_ENV/bin/activate
+
+        _setup-venv() {
             pip install -r requirements.txt
-        else
-            source $VIRTUAL_ENV/bin/activate
-        fi
+        }
+        ensure-venv _setup-venv
         
         alias start-brltty='sudo killall brltty; sudo $(which brltty) -f ./brltty.conf -n -p /var/run/brltty.pid'
         alias start-receiver="python receiver.py server"
-        alias | grep start
+
+        echo-shortcuts ${__curPos.file}
     '';
 }
